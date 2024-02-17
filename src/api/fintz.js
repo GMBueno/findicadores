0f24de0f182f9ff193b64ddfba5042a2a5c2e017
gmbueno <gmbueno1802@gmail.com>
2024-02-16 23:40:16 -0300

    minor changes

--- src/api/fintz.js
+++ src/api/fintz.js
@@ -1,15 +1,13 @@
 // src/api/stockService.js
 
 const BASE_URL = 'https://api.fintz.com.br'
-const COMMON_HEADERS = { 'X-API-Key': 'chave-de-teste-api-fintz' }
+const HEADERS = { 'X-API-Key': 'chave-de-teste-api-fintz' }
 
 class Fintz {
   static async getFintzIndicadores(ticker) {
-    const URL_BASE = 'https://api.fintz.com.br';
-    const HEADERS = { 'X-API-Key': 'chave-de-teste-api-fintz' };
     const PARAMS = new URLSearchParams({ ticker: ticker.toUpperCase() });
     
-    const endpoint = `${URL_BASE}/bolsa/b3/avista/indicadores/por-ticker?${PARAMS.toString()}`;
+    const endpoint = `${BASE_URL}/bolsa/b3/avista/indicadores/por-ticker?${PARAMS.toString()}`;
     
     try {
       const response = await fetch(endpoint, { headers: HEADERS });
@@ -21,12 +19,25 @@ class Fintz {
     }
   }
 
+  static async getFintzItensContabeis(ticker) {
+    const PARAMS = new URLSearchParams({ ticker: ticker.toUpperCase() });
+
+    const endpoint = `${BASE_URL}/bolsa/b3/avista/itens-contabeis/por-ticker?${PARAMS.toString()}`;
+
+    try {
+      const response = await fetch(endpoint, { headers: HEADERS });
+      const data = await response.json();
+      return data; // Return the fetched data for further processing
+    } catch (error) {
+      console.error("Failed to fetch stock items:", error);
+      throw error; // Rethrow the error to handle it in the calling function
+    }
+  }
+
   static async getFintzCotacoes(ticker, startDate = '2010-01-01') {
-    const URL_BASE = 'https://api.fintz.com.br';
-    const HEADERS = { 'X-API-Key': 'chave-de-teste-api-fintz' };
     const PARAMS = new URLSearchParams({ ticker: ticker.toUpperCase(), dataInicio: startDate });
 
-    const endpoint = `${URL_BASE}/bolsa/b3/avista/cotacoes/historico?${PARAMS.toString()}`;
+    const endpoint = `${BASE_URL}/bolsa/b3/avista/cotacoes/historico?${PARAMS.toString()}`;
 
     try {
       const response = await fetch(endpoint, { headers: HEADERS });
@@ -38,24 +49,6 @@ class Fintz {
     }
   }
 
-  static async getFintzItensContabeis(ticker) {
-    const URL_BASE = 'https://api.fintz.com.br';
-    const HEADERS = { 'X-API-Key': 'chave-de-teste-api-fintz' };
-    const PARAMS = new URLSearchParams({ ticker: ticker.toUpperCase() });
-
-    const endpoint = `${URL_BASE}/bolsa/b3/avista/itens-contabeis/por-ticker?${PARAMS.toString()}`;
-
-    try {
-      const response = await fetch(endpoint, { headers: HEADERS });
-      const data = await response.json();
-      return data; // Return the data for further processing
-    } catch (error) {
-      console.error("Failed to fetch stock items:", error);
-      throw error; // Rethrow the error to handle it in the calling function
-    }
-  }
-
-  // You can add more static methods here for other API calls
 }
 
 export default Fintz
