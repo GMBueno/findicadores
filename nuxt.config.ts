28caa6ffd0dcdb29e0d4c0c9d143951c76c49126
Joao Meyer <1994meyer@gmail.com>
2024-03-21 18:37:38 -0300

    Removed some unused files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  devtools: { enabled: true },
-  modules: [
-    "nuxt-primevue",
-    "@nuxtjs/tailwindcss",
-  ]
-})
+  devtools: {
+    enabled: process.env.NODE_ENV !== "production",
+  },
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
+});
