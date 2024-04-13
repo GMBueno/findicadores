4ca948b9ce34fe2a6085b581685f414995b9b626
Joao Meyer <1994meyer@gmail.com>
2024-04-13 18:15:13 -0300

    Added auth0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  app: {
-    baseURL: '/',
-  },
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
   devtools: {
     enabled: process.env.NODE_ENV !== "production",
   },
-  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
+  auth: {
+    globalAppMiddleware: true,
+    provider: {
+      type: "authjs",
+    },
+  },
 });
