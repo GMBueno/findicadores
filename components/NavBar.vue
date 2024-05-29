c3ec0dd0b0a6ec776e0d9f7c86c67f08da7f2d0a
gmbueno <gmbueno1802@gmail.com>
2024-05-29 00:54:59 -0300

    visuals

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,31 +4,31 @@
   >
     <div class="flex items-center">
       <img src="@/assets/icone-positivo.png" alt="Logo" class="h-8 mr-2" />
-      <h2 class="text-2xl font-semibold cursor-default">findicadores.com.br</h2>
+      <h2 class="text-xl font-semibold cursor-default">findicadores.com.br</h2>
     </div>
-    <div class="flex items-center gap-3">
+    <div class="flex items-center gap-4">
+      <div class="p-inputgroup border border-slate-200 rounded-md overflow-hidden focus:border-blue-400">
+        <InputText
+          v-model="ticker"
+          placeholder="Digite o ticker (ex: BBAS3)"
+          @keypress.enter="goToTicker"
+          class="placeholder:normal-case w-52 uppercase py-1 pl-3 text-blue-400 focus:font-semibold placeholder:font-normal placeholder-zinc-400 rounded-none focus:outline-none focus:ring-0"
+        />
+        <span class="p-inputgroup-addon cursor-pointer bg-white rounded-none" @click="goToTicker">
+          <i class="pi pi-search text-blue-400"></i>
+        </span>
+      </div>
       <Button
         label="Login"
         @click="() => signIn('auth0')"
-        class="bg-slate-200 hover:bg-slate-300 border border-slate-400 text-zinc-700 text-center py-1 px-5"
+        class="bg-white flex items-center justify-center hover:bg-blue-200 border border-slate-200 text-zinc-400 text-center py-1 px-5"
         v-if="status === 'unauthenticated'"
       />
       <Button
         label="Logout"
         @click="() => signOut()"
-        class="bg-slate-200 hover:bg-slate-300 border border-slate-400 text-zinc-700 text-center py-1 px-5"
+        class="bg-white flex items-center justify-center hover:bg-blue-200 border border-slate-200 text-zinc-400 text-center py-1 px-5"
         v-if="status === 'authenticated'"
-      />
-      <InputText
-        v-model="ticker"
-        placeholder="Digite o código (ex: BBAS3)"
-        @keypress.enter="goToTicker"
-        class="mt-0 w-60 px-2 py-1 placeholder:normal-case uppercase bg-slate-200 border border-slate-400 text-zinc-700 placeholder-zinc-500"
-      />
-      <Button
-        label="Buscar"
-        @click="goToTicker"
-        class="bg-slate-200 hover:bg-slate-300 border border-slate-400 text-zinc-700 px-5 text-center py-1"
       />
     </div>
   </nav>
